7b7244c6664e4db8441ede2bfe8fcf2d969090de
Jemutai <janetjemutai19@gmail.com>
2024-06-06 09:05:40 +0300

    linear layout with view Binding

--- app/src/main/java/com/jemutai/simplexml/MainActivity.kt
+++ app/src/main/java/com/jemutai/simplexml/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.jemutai.simplexml
 
+
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import com.jemutai.simplexml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  val LOG_TAG = "UserInfo"
+
+    private  lateinit var  binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnregister.setOnClickListener {
+            val etfname = binding.etfname.editableText.toString()
+            val  etlname = binding.etlname.editableText.toString()
+            val  etemail = binding.etemail.editableText.toString()
+            Log.d(LOG_TAG, "First Name: $etfname, Last Name: $etlname, Email: $etemail")
+
+
+        }
+
 
     }
 }
--- app/src/main/java/com/jemutai/simplexml/login.kt
+++ app/src/main/java/com/jemutai/simplexml/login.kt
@@ -3,17 +3,15 @@ package com.jemutai.simplexml
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.jemutai.simplexml.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
 
-    private lateinit var   binding : ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_login)
 
 
 
